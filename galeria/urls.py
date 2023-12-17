8e005d17ffaa679e8659829182de37cb8ad07d44
Ricardo Felipe <ricardofelipecrvg@gmail.com>
2023-12-17 15:02:54 -0300

    Atualização de arquivos, criação de Partials, adicionando arquivos estáticos e uso do DRY

--- galeria/urls.py
+++ galeria/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from galeria.views import index
+from galeria.views import index, imagem
 
 urlpatterns = [
-    path('', index)
+    path('', index, name='home'),
+    path('imagem', imagem, name='imagem')
 ]
