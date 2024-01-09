9f3ef501790b50f10ba660fc07949a2ad367fd2a
Ricardo Felipe <ricardofelipecrvg@gmail.com>
2024-01-09 15:25:39 -0300

    Finalizando o projeeto

--- galeria/views.py
+++ galeria/views.py
@@ -3,10 +3,23 @@ from galeria.models import Fotografia
 
 
 def index(request):
-    fotografias = Fotografia.objects.all()
+    fotografias = Fotografia.objects.order_by(
+        "-data_foto").filter(publicada=True)
     return render(request, 'galeria/index.html', {"cards": fotografias})
 
 
 def imagem(request, foto_id):
     foto = get_object_or_404(Fotografia, pk=foto_id)
     return render(request, 'galeria/imagem.html', {"fotografia": foto})
+
+
+def buscar(request):
+    fotografias = Fotografia.objects.order_by(
+        "-data_foto").filter(publicada=True)
+
+    if 'buscar' in request.GET:
+        nome_a_buscar = request.GET['buscar']
+        if nome_a_buscar:
+            fotografias = fotografias.filter(nome__icontains=nome_a_buscar)
+
+    return render(request, 'galeria/buscar.html', {'cards': fotografias})
