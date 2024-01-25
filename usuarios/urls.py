e3404412f160ad2a870b920de169c1f486158cae
Ricardo Felipe <ricardofelipecrvg@gmail.com>
2024-01-25 18:46:32 -0300

    Pequena atualização renomeando alguns path's

--- usuarios/urls.py
+++ usuarios/urls.py
@@ -3,7 +3,6 @@ from usuarios.views import login, cadastro
 from galeria.views import index
 
 urlpatterns = [
-    path('', index, name='index'),
     path('login', login, name='login'),
     path('cadastro', cadastro, name='cadastro')
 ]
